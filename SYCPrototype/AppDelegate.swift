4c1a0c26d43a71fa24dce0e04e95b76f31555e36
Dmyro Romaniuk <ro.dmytro@gmail.com>
2017-02-14 23:28:30 +0200

    Initial Commit

---
+++ SYCPrototype/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SYCPrototype
+//
+//  Created by Dmytro Romaniuk on 2/14/17.
+//  Copyright © 2017 Dmytro Romaniuk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SYCPrototype/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SYCPrototype
+//
+//  Created by Dmytro Romaniuk on 2/14/17.
+//  Copyright © 2017 Dmytro Romaniuk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
