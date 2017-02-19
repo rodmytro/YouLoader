293ad732d60c62534091d37b85361eb21d4b3677
Dmyro Romaniuk <ro.dmytro@gmail.com>
2017-02-19 15:53:34 +0200

    added date/time to filename

--- SYCPrototype/FileUtils.swift
+++ SYCPrototype/FileUtils.swift
@@ -8,11 +8,22 @@ import Cocoa
 
 class FileUtils {
 
-    let DEFAULT_FILE_NAME = "You-Loader.mp4"
+    let DEFAULT_FILE_NAME = "You-Loader"
+    let MP4 = ".mp4"
+    
+    private func generateFilename() -> String {
+        let date = NSDate()
+        let calendar = NSCalendar.current
+        let hour = calendar.component(.hour, from: date as Date)
+        let minutes = calendar.component(.minute, from: date as Date)
+        let seconds = calendar.component(.second, from: date as Date)
+
+        return DEFAULT_FILE_NAME + String(hour) + String(minutes) + String(seconds) + MP4
+    }
     
     func writeDataToFile(data: Data) {
         let documentsUrl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0] as NSURL
-        let fileUrl = documentsUrl.appendingPathComponent(DEFAULT_FILE_NAME)
+        let fileUrl = documentsUrl.appendingPathComponent(generateFilename())
 
         writeDataToFile(data: data, path: fileUrl!)
     }
